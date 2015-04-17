bddadaffcc792b8bf969e5227a8315df36720006
HarryCordewener <admin@twilightdays.org>
2015-04-17 15:53:20 -0500

    The other training corpusses, and additional code for the main project.

--- P5-essays/myspellcheck.py
+++ P5-essays/myspellcheck.py
@@ -5,6 +5,7 @@
 import nltk
 import enchant
 from enchant.checker import SpellChecker
+from nltk.tokenize import word_tokenize
 from nltk.corpus import treebank
 from nltk.tokenize import TreebankWordTokenizer
 from nltk.corpus import wordnet as wn
@@ -32,7 +33,6 @@ class MySpellChecker():
 if __name__ == '__main__':
     filename = "P5/P5-original/high/11580.txt"
     f = open(filename, 'r')
-    text = "The scale of the monitoring carried out by the NSA has been revealed in documents made public by whistleblower Edward Snowden over the last two years. Some of those papers show the NSA tapped the net's backbone network to siphon off data. The backbone is made up of high-speed cables that link big ISPs and key transit points on the net."
     text = f.read()    
 
     spellerrors = 0
@@ -67,10 +67,47 @@ if __name__ == '__main__':
     print("Words: " + str(len(tokenized)))
     print("Sentences: " + str(len(sentencearray)))
 
-    ##sent = text.split()
+    print(nltk.pos_tag(tokenized))
 
-    ##for tree in rd_parser.parse(sent):
-      ##  print(tree)
+    ## Work from here must be done using logic found at: http://www.nltk.org/book/ch05.html
+    ## Note that the subject is: Do you agree or disagree with the following statement?
+    ## "In twenty years, there will be fewer cars in use than there are today." Use reasons and examples to support your answer.
+    ## 'cars', 'fewer', should be in there. Along with perhaps 'twenty' or '20'
 
+    ## Grades, 1 = low, 5 = high
+    ## 1a = Spelling Mistakes
+    score_1a = 5 - max(min(int((spellerrors / 5)),4),0)
+    print(score_1a)
+
+    ## 1b = Subject-Verb agreement - agreement with respect to person and number (singular/plural)
+    score_1b = 0
+
+    ## 1c = Verb tense / missing verb / extra verb - is verb tense used correctly? Is a verb missing,
+    ## e.g. an auxiliary? For example, in the example of low essay above, the sequence will
+    ## be not agree is incorrect. Normally the verb to be is not followed by another infinitival
+    ## verb, but either a participle or a progressive tense.
+    score_1c = 0
+
+    ## 1d =  Sentence formation - are the sentences formed properly? i.e. beginning and ending
+    ## properly, is the word order correct, are the constituents formed properly? are there
+    ## missing words or constituents (prepositions, subject, object etc.)?
+    score_1d = 0
+
+    ## 2. Semantics (meaning) / Pragmatics (general quality)
+    ## (a) Is the essay coherent? Does it make sense?
+    score_2a = 0
+
+    ## (b) Does the essay address the topic?
+    score_2b = 0
+
+    ## 3. Length of the essay:
+    ## (a) Is the length appropriate? At least 10 sentences were required. Longer essays are in
+    ## general considered better.
+    score_3a = min(max(int(1+((len(sentencearray)-10)/2)),1),5)
+    print(score_3a)
+
+    ## Final Score = 1a + 1b + 1c + 2 ∗ 1d + 2 ∗ 2a + 3 ∗ 2b + 2 ∗ 3a
+    score_final = score_1a + score_1b + score_1c + 2*score_1d + 2*score_2a + 3*score_2b + 2*score_3a
+    print(score_final)
 
 
